a54eb7cd1251a34adbe1af546b94830c0c93fe1e
MarinaNikodim <marinenik@yandex.ru>
2024-08-13 15:55:41 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+course='Python'
+total_tasks=12
+hours_spent=1.5
+average_time_execution=0.125
+print('курс:', course, ', количество задач:', total_tasks, ', затраченное время:', hours_spent, ', среднее время выполнения:', average_time_execution)
+
+
